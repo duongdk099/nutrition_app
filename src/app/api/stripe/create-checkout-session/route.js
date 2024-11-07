a450cc73603daccfb11f6a473dceab5a12080713
duongd <duongdk69@gmail.com>
2024-11-07 20:28:05 +0100

    fix bugs on create checkout session

--- src/app/api/stripe/create-checkout-session/route.js
+++ src/app/api/stripe/create-checkout-session/route.js
@@ -3,8 +3,8 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 export async function POST(req) {
     try {
-        req.headers.origin = 'http://localhost:3000';
-        console.log(req.headers.origin);
+      // TODO : Fix the issue with req.headers.origin
+      const website = 'https://nutritionscore.site';
         
       const session = await stripe.checkout.sessions.create({
         payment_method_types: ['card'],
@@ -19,8 +19,8 @@ export async function POST(req) {
           },
         ],
         mode: 'payment',
-        success_url: `${req.headers.origin}/payment-success`,
-        cancel_url: `${req.headers.origin}/payment-cancel`,
+        success_url: `${website}/payment-success`,
+        cancel_url: `${website}/payment-cancel`,
       });
       return new Response(JSON.stringify({ sessionId: session.id }), { status: 200 });
     } catch (error) {
